0ca16d1b473aa9b934b971b688a59bd16970a3b7
mateomartinezmv <mateomartinezmv@gmail.com>
2026-03-11 10:53:09 -0300

    cortar

--- api/meli/auth.js
+++
@@ -1,9 +0,0 @@
-// api/meli/auth.js
-// GET /api/meli/auth → redirige al login de Mercado Libre
-
-module.exports = (req, res) => {
-  const clientId = process.env.MELI_CLIENT_ID;
-  const redirectUri = encodeURIComponent(process.env.MELI_REDIRECT_URI);
-  const url = `https://auth.mercadolibre.com.uy/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
-  res.redirect(url);
-};
